18fc0de27b23f25578ca183c087bf3f453496f17
StevenLignereux <steven.lignereux@hotmail.fr>
2018-11-06 17:51:19 +0100

    modals

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // Open modal
+  $('#open_modal').click(function() {
+    $('#modal_to_open').css(
+      {
+        'display':'block'
+      }
+    );
+  });
+});
