abe825a8d1d2c45ed4606e38b8c29e07331e2b27
StevenLignereux <steven.lignereux@hotmail.fr>
2018-11-07 09:40:08 +0100

    modals and ajax form

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,56 @@ $(document).ready(function() {
       }
     );
   });
+  // Close modal
+  $('#close_modal').click(function() {
+    $('#modal_to_open').css(
+      {
+        'display': 'none'
+      }
+    )
+  });
+
+  //send email with ajax
+  	$('#sending_email').click(function (e) {
+  	  e.preventDefault();
+  	  var data = {
+  	    email: $('#email').val(),
+  	    firstname: $('#firstname').val(),
+  	    name: $('#name').val(),
+  	    message: $('#message').val()
+  	  };
+  	  $.ajax({
+  	    url: "mail.php",
+  	    type: 'POST',
+  	    data: data,
+  	    success: function (data) {
+  	      $('#js_alert_success').css({
+  	        'display': 'block'
+  	      });
+  	      setTimeout(function () {
+  	        $('#js_alert_success').css({
+  	          'display': 'none'
+  	        });
+  	        $('#email').val("");
+  	        $('#name').val("");
+  	        $('#firstname').val("");
+  	        $('#message').val("")
+  	      }, 3000);
+  	    },
+  	    error: function (data) {
+  	      $('#js_alert_danger').css({
+  	        'display': 'block'
+  	      });
+  	      setTimeout(function () {
+  	        $('#js_alert_danger').css({
+  	          'display': 'none'
+  	        });
+  	        $('#email').val("");
+  	        $('#name').val("");
+  	        $('#firstname').val("");
+  	        $('#message').val("")
+  	      }, 3000);
+  	    }
+  	  });
+  	});
 });
